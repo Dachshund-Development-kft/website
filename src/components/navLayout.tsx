894a2b4a81aa9fd1aca9166c6d985921976179b3
Baluka0013 <baluka0013@gmail.com>
2025-01-28 17:46:37 +0100

    feat: enhance navigation layout with improved mobile menu functionality and z-index adjustment

--- src/components/navLayout.tsx
+++ src/components/navLayout.tsx
@@ -4,11 +4,11 @@ import { HiMiniHome } from "react-icons/hi2";
 import { FaDiagramProject } from "react-icons/fa6";
 import { HiUserAdd } from "react-icons/hi";
 import { FaGithub } from 'react-icons/fa';
-import { HiOutlineMenu } from "react-icons/hi"; // Hamburger icon
+import { HiOutlineMenu } from "react-icons/hi";
 
 const NavLayout: React.FC = () => {
     const location = useLocation();
-    const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage the menu
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const navItems = [
         { path: "/", icon: <HiMiniHome />, label: "Home" },
@@ -17,22 +17,18 @@ const NavLayout: React.FC = () => {
     ];
 
     return (
-        <div className="flex flex-row p-2 items-center bg-[#1A1B22] bg-opacity-50">
+        <div className="flex flex-row p-2 items-center bg-[#1A1B22] bg-opacity-50 relative">
             <div className="flex items-center">
                 <Link to="/" className="text-white">
                     <img width={50} src="./Logo.svg" alt="Logo" title="Logo" />
                 </Link>
             </div>
-
-            {/* Hamburger Icon for mobile */}
             <div className="lg:hidden ml-auto">
                 <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-white">
                     <HiOutlineMenu size={30} />
                 </button>
             </div>
-
-            {/* Navigation Menu (Sliding from Right for mobile) */}
-            <nav className={`flex flex-col items-start gap-8 p-4 fixed top-16 right-0 bg-[#1A1B22] max-w-max w-auto h-auto transition-all duration-300 transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} lg:translate-x-0 lg:flex-row lg:static lg:w-auto lg:h-auto lg:bg-transparent`}>
+            <nav className={`flex flex-col items-start gap-8 p-4 fixed top-16 right-0 bg-[#1A1B22] max-w-max w-auto h-auto transition-all duration-300 transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} lg:translate-x-0 lg:flex-row lg:static lg:w-auto lg:h-auto lg:bg-transparent z-50`}>
                 <div className="flex flex-col items-start gap-4">
                     {navItems.map((item, index) => {
                         const isActive = location.pathname === item.path;
@@ -59,8 +55,6 @@ const NavLayout: React.FC = () => {
                     })}
                 </div>
             </nav>
-
-            {/* Login Button */}
             <div className="flex items-center">
                 <button className="bg-[#0F1015] text-white px-4 py-2 rounded-md shadow-lg hover:bg-gray-700 flex items-center gap-2 transition-all duration-300">
                     <FaGithub /> Login
