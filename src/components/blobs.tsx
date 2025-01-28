cb1dfec6b19b9cc89b81d710ad0e7b5923f23c73
devbeni <ballobeni08@gmail.com>
2025-01-28 08:34:35 +0100

    Adjust blobBottom image position for improved layout

--- src/components/blobs.tsx
+++ src/components/blobs.tsx
@@ -4,7 +4,6 @@ const Blobs: React.FC = () => {
     const [pageCenter, setPageCenter] = useState(0);
 
     useEffect(() => {
-        // Calculate the center of the full page height (scrollable height)
         const center = document.documentElement.scrollHeight / 2;
         setPageCenter(center);
     }, []);
@@ -12,7 +11,7 @@ const Blobs: React.FC = () => {
         <div>
             <img src="./blob.svg" alt="Blob" style={{ position: 'absolute', top: -635, left:-700, zIndex: -1, width: '1200px', filter: 'blur(600px)', opacity:0.25 }} /> 
             <img src="./blob.svg"alt="Blob"style={{  position: 'absolute', top: `${pageCenter}px`,  right: -700, zIndex: -1, width: '1200px', filter: 'blur(600px)',opacity: 0.25,transform: 'translateY(-50%)',}} />
-            <img src="./blobBottom.svg" alt="Blob" style={{ position: 'absolute', top: `${pageCenter+pageCenter-635}px`, left:-700, zIndex: -1, width: '1200px', filter: 'blur(600px)', opacity:0.25 }} /> 
+            <img src="./blobBottom.svg" alt="Blob" style={{ position: 'absolute', top: `${pageCenter+pageCenter-100}px`, left:-700, zIndex: -1, width: '1200px', filter: 'blur(600px)', opacity:0.25 }} /> 
         </div>
     );
 };
