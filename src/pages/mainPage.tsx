34bf4d74921cde77b5d98a437feef295e1fe9783
Baluka0013 <baluka0013@gmail.com>
2025-01-28 18:37:27 +0100

    feat: update main page layout and background image for improved aesthetics and responsiveness

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -7,39 +7,35 @@ const MainPage: React.FC = () => {
     return (
         <div className="flex flex-col min-h-screen text-white">
             <NavLayout />
-            <main className="flex-grow items-center">
+            <main className="flex-grow items-center mt-24 md:mt-0">
                 <div className="flex flex-col items-center justify-center h-screen">
-                <div className="flex flex-col md:flex-row items-center mb-10 ml-0 md:ml-40 space-y-6 md:space-y-0 sm:mt-40 md:mt-0">
-    <div className="relative w-full md:w-auto flex justify-center md:justify-start">
-        <img
-            src="/backgroundImage.svg"
-            alt="Background Image"
-            className="absolute inset-0 w-full h-full object-cover opacity-50" />
-        <img
-            src="/mainPageLogo.svg"
-            alt="Dachshund Logo"
-            className="relative w-40 h-40 z-10" />
-    </div>
-    <div className="w-full md:w-auto flex flex-col text-center md:text-left pl-0 md:pl-2">
-        <h1 className="text-4xl font-bold bg-gradient-to-r from-[#5E78FF] to-[#384899] text-transparent bg-clip-text">
-            <span>Dachshund</span><br />
-            <span className="text-white">Development</span>
-        </h1>
-        <p className="text-sm text-gray-300 italic mt-2">
-            Sigma app, sigma dolgok...
-        </p>
-    </div>
-</div>
+                    <div className="flex flex-col md:flex-row items-center mb-10 ml-0 md:ml-24 space-y-6 md:space-y-0 md:mt-0">
+                        <div className="relative w-full md:w-auto flex justify-center md:justify-start">
+                            <img
+                                src="/mainPageLogo.svg"
+                                alt="Dachshund Logo"
+                                className="relative w-40 h-40 z-10" />
+                        </div>
+                        <div className="w-full md:w-auto flex flex-col text-center md:text-left pl-0 md:pl-2">
+                            <h1 className="text-4xl font-bold bg-gradient-to-r from-[#5E78FF] to-[#384899] text-transparent bg-clip-text">
+                                <span>Dachshund</span><br />
+                                <span className="text-white">Development</span>
+                            </h1>
+                            <p className="text-gray-300 italic mt-2 text-2xl">
+                                Sigma app, sigma dolgok...
+                            </p>
+                        </div>
+                    </div>
 
 
-                    <div className="bg-[#291E7C] rounded-2xl shadow-xl p-8 max-w-md text-center" style={{ backgroundColor: 'rgba(41, 30, 124, 0.12)' }}>
+                    <div className="bg-[#291E7C] rounded-2xl shadow-xl p-8  sm:max-w-md md:max-w-2xl text-center md:pl-24 md:pr-24 md:pt-12 md:pb-12" style={{ backgroundColor: 'rgba(41, 30, 124, 0.12)' }}>
                         <h2 className="text-4xl font-bold bg-gradient-to-r from-[#5E78FF] to-[#384899] text-transparent bg-clip-text">Rólunk</h2>
-                        <p className="text-sm text-gray-300 mb-4">
+                        <p className="md:text-lg sm:text-sm text-gray-300 mb-4 text-left">
                             A Dachshund Development elkötelezett az innovatív és hatékony informatikai
                             megoldások iránt. Célunk, hogy segítsük ügyfeleinket a digitális térben való fejlődésben
                             modern technológiákkal és szakértelemmel.
                         </p>
-                        <ul className="text-sm text-gray-400 text-left list-disc pl-6">
+                        <ul className="md:text-lg sm:text-sm text-gray-400 text-left list-disc pl-6">
                             <li>
                                 <span className="text-white font-semibold">Webfejlesztés:</span> Felhasználóbarát weboldalak és alkalmazások.
                             </li>
@@ -50,10 +46,11 @@ const MainPage: React.FC = () => {
                                 <span className="text-white font-semibold">IT-infrastruktúra menedzsment:</span> Stabil rendszerek tervezése és üzemeltetése.
                             </li>
                         </ul>
-                        <p className="text-sm text-gray-300 mt-4">
+                        <p className="md:text-lg sm:text-sm text-gray-300 mt-4 text-left">
                             Hiszünk abban, hogy az informatika az innováció és a fejlődés kulcsa. Fedezzük fel együtt a digitális lehetőségeket!
                         </p>
                     </div>
+
                 </div>
             </main>
             <FooterLayout />
