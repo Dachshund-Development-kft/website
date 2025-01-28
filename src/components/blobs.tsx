4a430973cc7277e0cc03bb10ab48d21c5ad73834
devbeni <ballobeni08@gmail.com>
2025-01-28 15:54:09 +0100

    sigma

--- src/components/blobs.tsx
+++ src/components/blobs.tsx
@@ -7,10 +7,12 @@ const Blobs: React.FC = () => {
         const center = window.innerHeight / 2;
         setPageCenter(center);
     }, []);
+
     return (
-        <div>
-            <img src="./blob.svg" alt="Blob" style={{ position: 'absolute', top: -635, left:-700, zIndex: -1, width: '1200px', filter: 'blur(600px)', opacity:0.25, overflow: 'hidden' }} /> 
-            <img src="./blob.svg"alt="Blob"style={{  position: 'absolute', top: `${pageCenter}px`,  right: -700, zIndex: -1, width: '1200px', filter: 'blur(600px)',opacity: 0.25,transform: 'translateY(-50%)', overflow: 'hidden'}} />
+        <div className='overflow-hidden'>
+            <img src="./blob.svg" alt="Blob" style={{ position: 'fixed', top: -635, left: -700, zIndex: -1, width: '1200px', filter: 'blur(600px)', opacity: 0.25 }} />
+            <img src="./blob.svg" alt="Blob" style={{ position: 'fixed', top: `${pageCenter}px`, right: -700, zIndex: -1, width: '1200px', filter: 'blur(600px)', opacity: 0.25, transform: 'translateY(-50%)' }} />
+            <img src="./blobBottom.svg" alt="Blob" style={{ position: 'fixed', top: `${pageCenter + pageCenter - 435}px`, left: -700, zIndex: -1, width: '1200px', filter: 'blur(600px)', opacity: 0.25 }} />
         </div>
     );
 };
