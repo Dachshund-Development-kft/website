7037842b2b903f2155d7e137872eec759de9d9bf
devbeni <ballobeni08@gmail.com>
2025-01-28 16:30:31 +0100

    refactor: simplify blob component and update footer margin

--- src/components/blobs.tsx
+++ src/components/blobs.tsx
@@ -1,18 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const Blobs: React.FC = () => {
-    const [pageCenter, setPageCenter] = useState(0);
-
-    useEffect(() => {
-        const center = window.innerHeight / 2;
-        setPageCenter(center);
-    }, []);
-
     return (
         <div className='overflow-hidden'>
-            <img src="./blob.svg" alt="Blob" style={{ position: 'fixed', top: -635, left: -700, zIndex: -1, width: '1200px', filter: 'blur(600px)', opacity: 0.25 }} />
-            <img src="./blob.svg" alt="Blob" style={{ position: 'fixed', top: `${pageCenter}px`, right: -700, zIndex: -1, width: '1200px', filter: 'blur(600px)', opacity: 0.25, transform: 'translateY(-50%)' }} />
-            <img src="./blobBottom.svg" alt="Blob" style={{ position: 'fixed', top: `${pageCenter + pageCenter - 435}px`, left: -700, zIndex: -1, width: '1200px', filter: 'blur(600px)', opacity: 0.25 }} />
+            <img
+                src="./blob.svg"
+                alt="Blob"
+                className="absolute top-[-635px] left-[-700px] w-[1200px] filter blur-[600px] opacity-25 z-[-2]"
+            />
+            <img
+                src="./blob.svg"
+                alt="Blob"
+                className="absolute top-[50%] right-[-700px] w-[1200px] filter blur-[600px] opacity-25 transform -translate-y-[50%] z-[-2]"
+            />
+            <img
+                src="./blobBottom.svg"
+                alt="Blob"
+                className="absolute top-[calc(50%+50%)] left-[-700px] w-[1200px] filter blur-[600px] opacity-25 z-[-2]"
+            />
         </div>
     );
 };
--- src/components/footerLayout.tsx
+++ src/components/footerLayout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FooterLayout: React.FC = () => {
   return (
-    <footer className="w-full relative z-10">
+    <footer className="w-full relative z-10 mt-32">
       <img src="/footer.svg" alt="Footer" className="w-full" />
     </footer>
   );
