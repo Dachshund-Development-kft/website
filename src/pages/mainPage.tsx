75a71623f6d93f81d4419a303089bf097c9e470d
Baluka0013 <baluka0013@gmail.com>
2025-01-31 14:56:37 +0100

    fix: adjust margin on main content for improved layout spacing

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -6,7 +6,7 @@ const MainPage: React.FC = () => {
     return (
         <div className="flex flex-col min-h-screen text-white inset-0 bg-cover filter z-[-2]" style={{ backgroundImage: "url(/blobs.svg)" }}>
             <NavLayout />
-            <main className="flex-grow items-center mt-40 lg:mt-64 xl:mt-72 2xl:mt-44 2xl:mb-48">
+            <main className="flex-grow items-center mt-64 lg:mt-64 xl:mt-72 2xl:mt-60 2xl:mb-48">
                 <div className="flex flex-col items-center justify-center h-screen">
                     <div className="flex flex-col md:flex-row items-center mb-10 ml-0 md:ml-24 space-y-6 md:space-y-0 md:mt-0 lg:mb-48">
                         <div className="relative w-full md:w-auto flex justify-center md:justify-start">
