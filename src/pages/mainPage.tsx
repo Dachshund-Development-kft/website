3a3ddb2ba5e30056212be0bf186d36d1a8e755f4
Baluka0013 <baluka0013@gmail.com>
2025-02-03 22:32:49 +0100

    Add responsive partner display sections for mobile and desktop

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import NavLayout from '../components/navLayout';
 import FooterLayout from '../components/footerLayout';
+import '../css/footerAnimation.css';
+import partnersDesktop from '../data/partners desktop.json';
+import partnersMobile from '../data/partners phone.json';
 
 const MainPage: React.FC = () => {
     return (
         <div className="flex flex-col min-h-screen text-white inset-0 bg-cover filter z-[-2] bg-repeat-y" style={{ backgroundImage: "url(/blobs.svg)" }}>
             <NavLayout />
-            <main className="flex-grow items-center mt-64 lg:mt-64 xl:mt-72 2xl:mt-60 2xl:mb-48 mb-20 lg:mb-0">
-                <div className="flex flex-col items-center justify-center h-screen">
+            <main className="flex-grow items-center mt-96 lg:mt-96 xl:mt-96 2xl:mt-80 2xl:mb-48 mb-20 lg:mb-0">
+                <div className="flex flex-col items-center mt-36 justify-center h-screen">
                     <div className="flex flex-col md:flex-row items-center mb-10 ml-0 md:ml-24 space-y-6 md:space-y-0 md:mt-0 lg:mb-48">
                         <div className="relative w-full md:w-auto flex justify-center md:justify-start">
                             <img
@@ -48,7 +51,44 @@ const MainPage: React.FC = () => {
                             Hiszünk abban, hogy az informatika az innováció és a fejlődés kulcsa. Fedezzük fel együtt a digitális lehetőségeket!
                         </p>
                     </div>
-
+                    <div id='scroll' className="text-center mb-52 block lg:hidden" style={{ width: "100%"}}>
+                        <h2 className="text-4xl font-bold bg-gradient-to-r from-[#5E78FF] to-[#384899] text-transparent bg-clip-text my-10">Partnereink</h2>
+                        <div className="relative w-full h-96 overflow-hidden">
+                        {partnersMobile.map((partner, index) => (
+                                <div key={index} className={`footer-bg footer_bg${index + 1} footerphone`}>
+                                    <a href={partner.url}>
+                                        <div className="max-w-56 rounded-2xl overflow-hidden shadow-lg bg-[#291E7C] bg-opacity-50">
+                                            <div className='m-8'>
+                                                <img className="w-full" src={partner.logo} alt={partner.name} />
+                                            </div>
+                                            <div className="px-6 py-4">
+                                                <div className="font-bold text-xl mb-2">{partner.name}</div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div id='scroll' className="text-center mb-32 lg:block hidden" style={{ width: "130%" }}>
+                        <h2 className="text-4xl font-bold bg-gradient-to-r from-[#5E78FF] to-[#384899] text-transparent bg-clip-text my-10">Partnereink</h2>
+                        <div className="relative w-full h-96 overflow-hidden">
+                            {partnersDesktop.map((partner, index) => (
+                                <div key={index} className={`footer-bg footerbg${index + 1}`}>
+                                    <a href={partner.url}>
+                                        <div className="max-w-56 rounded-2xl overflow-hidden shadow-lg bg-[#291E7C] bg-opacity-50">
+                                            <div className='m-8'>
+                                                <img className="w-full" src={partner.logo} alt={partner.name} />
+                                            </div>
+                                            <div className="px-6 py-4">
+                                                <div className="font-bold text-xl mb-2">{partner.name}</div>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </main>
             <FooterLayout />
